cb85a50a1a4bd3b92ba60c90de2dc289222c4269
mindguy <coachbolatunji@gmail.com>
2024-05-06 14:17:06 +0100

    updated to production database

--- database.py
+++ database.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://taskapp_wq5l_user:KafmTz4cjeyg8oaZ0OIjx0dyQBn5cZG2@dpg-cortici0si5c739mbigg-a.oregon-postgres.render.com/taskapp_wq5l"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
